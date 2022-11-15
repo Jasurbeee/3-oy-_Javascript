12ecde265bc7c6b7db493698133aafa78355c10c
jasurbee <jasurfakhriddinov@gmail.com>
2022-11-15 19:56:48 +0500

    first

---
+++ main.js
@@ -0,0 +1,34 @@
+"use strict";
+
+console.log(prompt("Ismimginzi kiriting"));
+console.log(prompt("Familiyangizni kiriting"));
+console.log(prompt("Adresingnizni kiriting kiriting"));
+console.log(prompt("Telefon raqamingizni kiriting"));
+console.log(prompt("Email adresingizni kiriting"));
+console.log(prompt("Yoshingizni kiriting kiriting"));
+console.log(prompt("Jinsingizni kiriting"));
+console.log(prompt("Yoqtirgan rangingizni kiriting"));
+
+let ism = prompt("ismingizni kiriting");
+let familiya = prompt("familiyangizni kiriting");
+let adres = prompt("Adresingizni kiriting");
+let kasb = prompt("Kasbingizni kiriting");
+let maktab = prompt("O'qigan maktabingizni kiriting");
+let tugJoy = prompt("Tug'ilgan joyingizni kiriting");
+let univer = prompt("Oqigan universitetingizni kiriting");
+
+console.log(
+  `Mening ismim ${ism} familiyam ${familiya}. ${tugJoy} da tugilganman. ${maktab} da tahsil olganman,  Hozirgi kunda ${adres}da yashayapman. ${univer} ni tamomlaganman, kasbim ${kasb}.  Tanishganimdan hursandman`
+);
+
+let a = 10;
+let b = 5;
+console.log(a + b);
+
+let c = 25;
+let d = 10;
+let result = c - d;
+console.log(result);
+
+let f = 2;
+console.log(f ** 10);
