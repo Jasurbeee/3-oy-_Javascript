238d9626635f011de490f4e6d6f2c8b8ae6c774a
jasurbee <jasurfakhriddinov@gmail.com>
2022-11-15 21:00:12 +0500

    first

--- main.js
+++ main.js
@@ -1,34 +1,47 @@
 "use strict";
 
-console.log(prompt("Ismimginzi kiriting"));
-console.log(prompt("Familiyangizni kiriting"));
-console.log(prompt("Adresingnizni kiriting kiriting"));
-console.log(prompt("Telefon raqamingizni kiriting"));
-console.log(prompt("Email adresingizni kiriting"));
-console.log(prompt("Yoshingizni kiriting kiriting"));
-console.log(prompt("Jinsingizni kiriting"));
-console.log(prompt("Yoqtirgan rangingizni kiriting"));
+// console.log(prompt("Ismimginzi kiriting"));
+// console.log(prompt("Familiyangizni kiriting"));
+// console.log(prompt("Adresingnizni kiriting kiriting"));
+// console.log(prompt("Telefon raqamingizni kiriting"));
+// console.log(prompt("Email adresingizni kiriting"));
+// console.log(prompt("Yoshingizni kiriting kiriting"));
+// console.log(prompt("Jinsingizni kiriting"));
+// console.log(prompt("Yoqtirgan rangingizni kiriting"));
 
-let ism = prompt("ismingizni kiriting");
-let familiya = prompt("familiyangizni kiriting");
-let adres = prompt("Adresingizni kiriting");
-let kasb = prompt("Kasbingizni kiriting");
-let maktab = prompt("O'qigan maktabingizni kiriting");
-let tugJoy = prompt("Tug'ilgan joyingizni kiriting");
-let univer = prompt("Oqigan universitetingizni kiriting");
+// let ism = prompt("ismingizni kiriting");
+// let familiya = prompt("familiyangizni kiriting");
+// let adres = prompt("Adresingizni kiriting");
+// let kasb = prompt("Kasbingizni kiriting");
+// let maktab = prompt("O'qigan maktabingizni kiriting");
+// let tugJoy = prompt("Tug'ilgan joyingizni kiriting");
+// let univer = prompt("Oqigan universitetingizni kiriting");
+
+// console.log(
+//   `Mening ismim ${ism} familiyam ${familiya}. ${tugJoy} da tugilganman. ${maktab} da tahsil olganman,  Hozirgi kunda ${adres}da yashayapman. ${univer} ni tamomlaganman, kasbim ${kasb}.  Tanishganimdan hursandman`
+// );
+
+// let a = 10;
+// let b = 5;
+// console.log(a + b);
+
+// let c = 25;
+// let d = 10;
+// let result = c - d;
+// console.log(result);
+
+// let f = 2;
+// console.log(f ** 10);
+
+const aviaBilet = 500;
+const hotel = 250;
+const museums = 120; //yevro
+
+const jamiHarajat =
+  aviaBilet * 10650.34 + hotel * 10650.34 + museums * 11650.03;
+
+const sum = prompt("Qancha pulingiz borligini kiriting ");
 
 console.log(
-  `Mening ismim ${ism} familiyam ${familiya}. ${tugJoy} da tugilganman. ${maktab} da tahsil olganman,  Hozirgi kunda ${adres}da yashayapman. ${univer} ni tamomlaganman, kasbim ${kasb}.  Tanishganimdan hursandman`
+  `Sizning ${sum} so'm pulimgiz bor. Sayoht uchun jami harajat esa ${jamiHarajat} ni tashkil etadi`
 );
-
-let a = 10;
-let b = 5;
-console.log(a + b);
-
-let c = 25;
-let d = 10;
-let result = c - d;
-console.log(result);
-
-let f = 2;
-console.log(f ** 10);
