3628c7a5d9f420abb242caaf3be1a21404bc2596
jasurbee <jasurfakhriddinov@gmail.com>
2022-11-17 21:19:57 +0500

    main

---
+++ 2-dars.js
@@ -0,0 +1,82 @@
+// let a = prompt("son");
+
+// let split = a.split("");
+// console.log(split);
+// let reverse = split.reverse();
+// let join = reverse.join("");
+
+// let num = Number(join);
+
+// console.log(num);
+
+// let A = 1015;
+// let B = 653;
+
+// a = (A + B) / 2 - 1;
+// b = (A + B) / 2 + 1;
+
+// console.log(a);
+// console.log(b);
+
+// Yilning istalgan kuni haftaning qaysi kuniga togri keladi
+
+let K = prompt("biror kun kiriting");
+
+if (K % 7 == 0) {
+  console.log(`Yilning ${K} inchi kuni  Yakshanbaga tog'ri kelmoqda`);
+} else if (K % 7 == 1) {
+  console.log(`Yilning ${K} inchi kuni  Dushanbaga tog'ri kelmoqda`);
+} else if (K % 7 == 2) {
+  console.log(`Yilning ${K} inchi kuni  Seshanbaga tog'ri kelmoqda`);
+} else if (K % 7 == 3) {
+  console.log(`Yilning ${K} inchi kuni  Chorshanbaga tog'ri kelmoqda`);
+} else if (K % 7 == 4) {
+  console.log(`Yilning ${K} inchi kuni  Payshanbaga tog'ri kelmoqda`);
+} else if (K % 7 == 5) {
+  console.log(`Yilning ${K} inchi kuni  Jumaga tog'ri kelmoqda`);
+} else if (K % 7 == 6) {
+  console.log(`Yilning ${K} inchi kuni  Shanbaga tog'ri kelmoqda`);
+}
+
+// Sonlar berilgan ulardan nechtasi musbat yoki manfiyligini aniqlovchi kod
+
+// let a = 5;
+// let b = 6;
+// let c = -5;
+// let d = -8;
+// let f = 3;
+
+// let musbat = [];
+// let manfiy = [];
+
+// if (a > 0) {
+//   musbat.push(a);
+// } else {
+//   manfiy.push(a);
+// }
+// if (b > 0) {
+//   musbat.push(b);
+// } else {
+//   manfiy.push(b);
+// }
+
+// if (c > 0) {
+//   musbat.push(c);
+// } else {
+//   manfiy.push(c);
+// }
+
+// if (d > 0) {
+//   musbat.push(d);
+// } else {
+//   manfiy.push(d);
+// }
+
+// if (f > 0) {
+//   musbat.push(f);
+// } else {
+//   manfiy.push(f);
+// }
+
+// console.log(musbat.length);
+// console.log(manfiy.length);
